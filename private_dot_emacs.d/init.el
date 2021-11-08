2a3f89a6a645ff4c15c1088deca0e0fdd9b414be
Abhishek Chakraborty <abhishekchak52@gmail.com>
2021-11-07 21:53:26 -0500

    Initial Commit

---
+++ private_dot_emacs.d/init.el
@@ -0,0 +1,417 @@
+(setq inhibit-startup-message t)
+
+
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+
+(menu-bar-mode -1)            ; Disable the menu bar
+
+;; Set up the visible bell
+(setq visible-bell t)
+
+(setq efs/default-font-size 120)
+(setq efs/default-variable-font-size 100)
+
+(global-set-key (kbd "C-c C-b") 'eval-buffer)
+
+(defun efs/set-font-faces ()
+  (message "Setting faces!")
+  (set-face-attribute 'default nil :font "Cascadia Code" :height efs/default-font-size)
+
+  ;; Set the fixed pitch face
+  (set-face-attribute 'fixed-pitch nil :font "Cascadia Code" :height efs/default-font-size)
+
+  ;; Set the variable pitch face
+  (set-face-attribute 'variable-pitch nil :font "Cascadia Code" :height efs/default-variable-font-size :weight 'regular))
+
+(if (daemonp)
+    (add-hook 'after-make-frame-functions
+              (lambda (frame)
+                (setq doom-modeline-icon t)
+                (with-selected-frame frame
+                  (efs/set-font-faces))))
+  (efs/set-font-faces))
+
+;; Make ESC quit prompts
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+(column-number-mode)
+
+(global-display-line-numbers-mode t)
+
+;; Enable line numbers for some modes
+(dolist (mode '(text-mode-hook
+                prog-mode-hook
+                conf-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 1))))
+
+;; Override some modes which derive from the above
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(use-package command-log-mode)
+
+(use-package ligature
+ :straight (ligature :type git :host github :repo "mickeynp/ligature.el")
+ :config
+ ;; Enable the "www" ligature in every possible major mode
+ (ligature-set-ligatures 't '("www"))
+ ;; Enable traditional ligature support in eww-mode, if the
+ ;; `variable-pitch' face supports it
+ (ligature-set-ligatures 'eww-mode '("ff" "fi" "ffi"))
+ ;; Enable all Cascadia Code ligatures in programming modes
+ (ligature-set-ligatures 'prog-mode '("|||>" "<|||" "<==>" "<!--" "####" "~~>" "***" "||=" "||>"
+                                      ":::" "::=" "=:=" "===" "==>" "=!=" "=>>" "=<<" "=/=" "!=="
+                                      "!!." ">=>" ">>=" ">>>" ">>-" ">->" "->>" "-->" "---" "-<<"
+                                      "<~~" "<~>" "<*>" "<||" "<|>" "<$>" "<==" "<=>" "<=<" "<->"
+                                      "<--" "<-<" "<<=" "<<-" "<<<" "<+>" "</>" "###" "#_(" "..<"
+                                      ;; "..." "+++" "/==" "///" "_|_" "www" "&&" "^=" "~~" "~@" "~="
+                                      "~>" "~-" "**" "*>" "*/" "||" "|}" "|]" "|=" "|>" "|-" "{|"
+                                      "[|" "]#" "::" ":=" ":>" ":<" "$>" "==" "=>" "!=" "!!" ">:"
+                                      ">=" ">>" ">-" "-~" "-|" "->" "--" "-<" "<~" "<*" "<|" "<:"
+                                      "<$" "<=" "<>" "<-" "<<" "<+" "</" "#{" "#[" "#:" "#=" "#!"
+                                      "##" "#(" "#?" "#_" "%%" ".=" ".-" ".." ".?" "+>" "++" "?:"
+                                      "?=" "?." "??" ";;" "/*" "/=" "/>" "//" "__" "~~" "(*" "*)"
+                                      "\\\\" "://"))
+ ;; Enables ligature checks globally in all buffers. You can also do it
+ ;; per mode with `ligature-mode'.
+ (global-ligature-mode t))
+
+(use-package sudo-edit)
+
+(use-package ivy
+  :diminish
+  :bind (("C-/" . evilnc-comment-or-uncomment-lines) 
+	 ("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)	
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history)))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1)
+  :after counsel)
+
+(use-package ivy-prescient
+  :after counsel
+  :config
+  (ivy-prescient-mode 1))
+
+
+(use-package all-the-icons)
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 40)))
+
+(use-package doom-themes
+  :config
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-molokai t)
+  
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-molokai") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-symbol] . helpful-symbol)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-key] . helpful-key))
+
+;; Completion
+(use-package company
+  :hook (after-init . global-company-mode))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll nil)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;; (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package evil-surround
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-nerd-commenter
+  :config
+  (evilnc-default-hotkeys))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+;; Math hydras
+
+(defhydra math-lowercase (:color blue :columns 6)
+  "Select your symbol below (also try C-c n)"
+  ("a" (lambda () (interactive) (insert "α")) "α")
+  ("b" (lambda () (interactive) (insert "β")) "β")
+  ("c" (lambda () (interactive) (insert "∊")) "∊")
+  ("d" (lambda () (interactive) (insert "δ")) "δ")
+  ("e" (lambda () (interactive) (insert "ε")) "ε")
+  ("f" (lambda () (interactive) (insert "φ")) "φ")
+  ("g" (lambda () (interactive) (insert "γ")) "γ")
+  ("h" (lambda () (interactive) (insert "θ")) "θ")
+  ("i" (lambda () (interactive) (insert "ι")) "ι")
+  ("j" (lambda () (interactive) (insert "ξ")) "ξ")
+  ("k" (lambda () (interactive) (insert "κ")) "κ")
+  ("l" (lambda () (interactive) (insert "λ")) "λ")
+  ("m" (lambda () (interactive) (insert "μ")) "μ")
+  ("n" (lambda () (interactive) (insert "η")) "η")
+  ("o" (lambda () (interactive) (insert "ο")) "ο")
+  ("p" (lambda () (interactive) (insert "π")) "π")
+  ("r" (lambda () (interactive) (insert "ρ")) "ρ")
+  ("s" (lambda () (interactive) (insert "σ")) "σ")
+  ("t" (lambda () (interactive) (insert "τ")) "τ")
+  ("u" (lambda () (interactive) (insert "υ")) "υ")
+  ("v" (lambda () (interactive) (insert "ν")) "ν")
+  ("w" (lambda () (interactive) (insert "ω")) "ω")
+  ("x" (lambda () (interactive) (insert "χ")) "χ")
+  ("y" (lambda () (interactive) (insert "ψ")) "ψ")
+  ("z" (lambda () (interactive) (insert "ζ")) "ζ")
+  ("," (lambda () (interactive) (insert "≤")) "≤")
+  ("." (lambda () (interactive) (insert "≥")) "≥")
+  ("=" (lambda () (interactive) (insert "≠")) "≠")
+  ("-" (lambda () (interactive) (insert "±")) "±")
+  ("0" (lambda () (interactive) (insert "∅")) "∅")
+  ("1" (lambda () (interactive) (insert "→")) "→")
+  ("2" (lambda () (interactive) (insert "↔")) "↔")
+  ("3" (lambda () (interactive) (insert "↦")) "↦")
+  ("4" (lambda () (interactive) (insert "↑")) "↑")
+  ("5" (lambda () (interactive) (insert "↓")) "↓")
+  ("6" (lambda () (interactive) (insert "↗")) "↗")
+  ("7" (lambda () (interactive) (insert "↘")) "↘")
+  ("8" (lambda () (interactive) (insert "∞")) "∞")
+  ("9" (lambda () (interactive) (insert "⋯")) "⋯")
+  ("q" nil "cancel"))
+
+;; Hydra - Upper Case
+
+(defhydra math-uppercase (:color blue :columns 6)
+  "Select your symbol below (also try C-c m)"
+  ("a" (lambda () (interactive) (insert "∀")) "∀")
+  ("b" (lambda () (interactive) (insert "∃")) "∃")
+  ("c" (lambda () (interactive) (insert "ℂ")) "ℂ")
+  ("d" (lambda () (interactive) (insert "Δ")) "Δ")
+  ("e" (lambda () (interactive) (insert "∈")) "∈")
+  ("f" (lambda () (interactive) (insert "Φ")) "Φ")
+  ("g" (lambda () (interactive) (insert "Γ")) "Γ")
+  ("h" (lambda () (interactive) (insert "Θ")) "Θ")
+  ("i" (lambda () (interactive) (insert "∫")) "∫")
+  ("j" (lambda () (interactive) (insert "∂")) "∂")
+  ("k" (lambda () (interactive) (insert "⊢")) "⊢")
+  ("l" (lambda () (interactive) (insert "Λ")) "Λ")
+  ("m" (lambda () (interactive) (insert "∄")) "∄")
+  ("n" (lambda () (interactive) (insert "ℕ")) "ℕ")
+  ("o" (lambda () (interactive) (insert "⊕")) "⊕")
+  ("p" (lambda () (interactive) (insert "Π")) "Π")
+  ("r" (lambda () (interactive) (insert "ℝ")) "ℝ")
+  ("s" (lambda () (interactive) (insert "Σ")) "Σ")
+  ("t" (lambda () (interactive) (insert "∴")) "∴")
+  ("u" (lambda () (interactive) (insert "∵")) "∵")
+  ("v" (lambda () (interactive) (insert "√")) "√")
+  ("w" (lambda () (interactive) (insert "Ω")) "Ω")
+  ("x" (lambda () (interactive) (insert "∊")) "∊")
+  ("y" (lambda () (interactive) (insert "Ψ")) "Ψ")
+  ("z" (lambda () (interactive) (insert "ℤ")) "ℤ")
+  ("," (lambda () (interactive) (insert "¬")) "¬")
+  ("." (lambda () (interactive) (insert "≡")) "≡")
+  ("=" (lambda () (interactive) (insert "≈")) "≈")
+  ("-" (lambda () (interactive) (insert "≠")) "≠")
+  ("0" (lambda () (interactive) (insert "∉")) "∉")
+  ("1" (lambda () (interactive) (insert "ℚ")) "ℚ")
+  ("2" (lambda () (interactive) (insert "⊂")) "⊂")
+  ("3" (lambda () (interactive) (insert "⊃")) "⊃")
+  ("4" (lambda () (interactive) (insert "⋂")) "⋂")
+  ("5" (lambda () (interactive) (insert "⋃")) "⋃")
+  ("6" (lambda () (interactive) (insert "∧")) "∧")
+  ("7" (lambda () (interactive) (insert "∨")) "∨")
+  ("8" (lambda () (interactive) (insert "∙")) "∙")
+  ("9" (lambda () (interactive) (insert "∘")) "∘")
+  ("q" nil "cancel"))
+
+
+;; Key Maps
+
+(global-set-key (kbd "C-c m") #'math-lowercase/body)
+(global-set-key (kbd "C-c n") #'math-uppercase/body)
+
+(use-package general
+  :config
+  (general-create-definer rune/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (rune/leader-keys
+    "f"  '(:ignore f :which-key "Files")
+    "ff" '(counsel-find-file :which-key "Open file")
+    "fr" '(counsel-recentf :which-key "Open file")
+    "fp" '(lambda() (interactive) (find-file "~/.emacs.d/init.el"))
+    "b"  '(:ignore b :which-key "Buffer")
+    "bk" '(kill-current-buffer :which-key "Kill buffer")
+    "bn" '(next-buffer :which-key "Next buffer")
+    "bp" '(previous-buffer :which-key "Previous buffer")
+    ","  '(counsel-switch-buffer :which-key "Switch buffer")
+    "w"  '(:ignore w :which-key "Window")
+    "wv" '(split-window-right :which-key "Vertical Split")
+    "ws" '(split-window-below  :which-key "Horizontal Split")
+    "wM" '(maximize-window :which-key "Maximize Window")
+    "wm" '(minimize-window :which-key "Minimize Window")
+    "wq" '(evil-quit :which-key "Close Window")
+    "q"  '(:ignore q :which-key "Quit")
+    "qq" '(kill-emacs :which-key "Quit Emacs")
+    "t"  '(:ignore t :which-key "toggles")
+    "tt" '(Counsel-load-theme :which-key "choose theme")
+    "ts" '(hydra-text-scale/body :which-key "scale text")))
+(general-evil-setup)
+
+(use-package vterm)
+
+(use-package pdf-tools)
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+;; Org related
+
+(use-package org
+  :straight '(org :type git
+		  :repo "https://git.savannah.gnu.org/git/emacs/org-mode.git")
+  :config
+  (setq org-export-date-timestamp-format "%A, %B %e, %Y"))
+
+(use-package org-contrib
+  :straight t)
+
+(use-package org-superstar :after org :hook (org-mode . org-superstar-mode))
+
+;; Python things
+
+(use-package conda
+  :config
+  (custom-set-variables '(conda-anaconda-home "~/mambaforge/"))
+  (setq conda-env-home-directory (expand-file-name "~/mambaforge/")))
+(conda-env-activate "base")
+
+(use-package jupyter)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (julia . t)
+   (python . t)
+   (jupyter . t)))
+
+;; HERE BE DRAGONS
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(org pdf-tools vterm counsel-projectile projectile ligature command-log-mode quelpa-use-package quelpa general hydra evil-nerd-commenter evil-surround evil-collection evil helpful which-key rainbow-delimiters doom-themes doom-modeline))
+ '(warning-suppress-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
